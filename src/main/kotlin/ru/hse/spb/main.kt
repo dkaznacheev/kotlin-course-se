bc4cd926e8a9d600919ae43f72ba16438ec01c97
dkaznacheev <dkaznacheev@github.com>
2018-09-05 23:34:05 +0300

    Added HW01, first version

--- src/main/kotlin/ru/hse/spb/main.kt
+++ src/main/kotlin/ru/hse/spb/main.kt
@@ -1,13 +1,50 @@
 package ru.hse.spb
 
-fun getGreeting(): String {
-    val words = mutableListOf<String>()
-    words.add("Hello,")
-    words.add("world!")
+import java.util.*
 
-    return words.joinToString(separator = " ")
+fun formattedName(lettersNumber: Int, template: String): String {
+    val letters: List<Char> = listOf('a'..'z')
+            .flatten()
+            .take(lettersNumber)
+            .minus(template.toList())
+
+    val name = template.toCharArray()
+
+    val n = template.length
+    var wildCards = 0
+
+    for (i in 0 until (n + 1) / 2) {
+        if (name[i] != name[n - i - 1]) {
+            when {
+                name[i] == '?' -> name[i] = name[n - i - 1]
+                name[n - i - 1] == '?' -> name[n - i - 1] = name[i]
+                else -> return "IMPOSSIBLE"
+            }
+        } else if (name[i] == '?')
+            wildCards++
+    }
+
+    if (wildCards < letters.size)
+        return "IMPOSSIBLE"
+
+    var offset = letters.size - wildCards
+
+    for (i in 0 until (n + 1) / 2) {
+        if (name[i] == '?') {
+            val letter = if (offset < 0) 'a' else letters[offset]
+            name[i] = letter
+            name[n - i - 1] = letter
+            offset++
+        }
+    }
+
+    return name.joinToString("")
 }
 
 fun main(args: Array<String>) {
-    println(getGreeting())
+    val scanner = Scanner(System.`in`)
+    val lettersNumber = scanner.nextInt()
+    val template = scanner.next()
+
+    println(formattedName(lettersNumber, template))
 }
--- src/test/kotlin/ru/hse/spb/test.kt
+++ src/test/kotlin/ru/hse/spb/test.kt
@@ -1,11 +1,34 @@
 package ru.hse.spb
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun testImpossible() {
+        assertEquals("IMPOSSIBLE", formattedName(3, "a?c"))
+    }
+
+    @Test
+    fun testSimple01() {
+        assertEquals("abba", formattedName(2, "a??a"))
+    }
+
+    @Test
+    fun testSimple02() {
+        assertEquals("abba", formattedName(2, "?b?a"))
+    }
+
+    @Test
+    fun testQuestionMark() {
+        assertEquals("a", formattedName(1, "?"))
+    }
+
+    @Test
+    fun testOrdered() {
+        assertEquals("abcaefahijklmnophbajliiglaljannpiadbbdaipnnajlalgiiljabhponmlkjihafeacba",
+                formattedName(16,
+                "?bc??f?hi?k??n??hb??liiglal???npi???b???p?n?j?al????j???ponmlkjih??e??b?"))
     }
 }
